2ec4b85197395ba3540714014ebd6ea95bc8e877
PUARI-Admin <95752521+PUARI-Admin@users.noreply.github.com>
2021-12-22 18:22:04 +0900

    Add files via upload

---
+++ Spring/server/puari/src/test/java/puari/web/PuariApplicationTests.java
@@ -0,0 +1,13 @@
+package puari.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
