06f46fdba153d9ecc942a72731515785713eed2b
ISHIKAWA <kjahdhebfu1255@gmail.com>
2022-03-01 21:36:22 +0900

    ログインモジュール追加

--- Angular/puari-app/src/app/app-routing.module.ts
+++ Angular/puari-app/src/app/app-routing.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-  }
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
+  { path:'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
+  { path: '', redirectTo: 'login', pathMatch:'full' }
 ];
 
 @NgModule({
